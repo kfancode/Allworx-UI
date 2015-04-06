f6fde3b6d905cb0f83c189195824932a6de4a7bc
kfancode <kfanion@gmail.com>
2015-04-06 16:10:58 -0400

    minor adjustments

--- AllworxCall Activity/billing.sql
+++ AllworxCall Activity/billing.sql
@@ -12,15 +12,12 @@ This is because the call length is either 0.1 or 0.  Multiply that by the 0.0250
 /***   Run this to get the domestic calls totals   ***/
 select cast(sum(duration) as numeric(7,2)) 'Minutes', round(sum(cast(duration as numeric(7,2)) * 0.0250),2) 'Our Total'
 from chargedcalls
---sum(convert(decimal(5,4),replace(charge, '$', ''))) 'Amount' from chargedcalls
 where
 calltype in ('domestic')
 group by calltype
---  and charge = '$0.00'
---calltype = 'incoming toll free'
---  and charge <> '$0.00'
 
 
+/***   run the following to file to create the tab delimited file to import into Excel   ***/
 select 
 calldate,
 calltime,
@@ -29,12 +26,10 @@ placedto,
 placedfrom,
 cast(duration as numeric(7,2)) 'Minutes',
 rate,
-
 case 
 when calltype = 'domestic' then duration * 0.0250
 when calltype = 'incoming toll free' then duration * 0.0400
 end 'Our Charge',
---duration * 0.0250 'Our Charge',
 acctcode
 from chargedcalls
 where calltype in ('Incoming Toll Free','domestic')
@@ -44,16 +39,6 @@ order by calltype
 
 
 
-
-Select sum(duration) from chargedcalls 
-where
-calltype = 'domestic'
---and duration = '0'
-  and charge = '$0.00'
-
-  select distinct calltype from chargedcalls
-
-  delete from chargedcalls where 1=1
 
  
 
